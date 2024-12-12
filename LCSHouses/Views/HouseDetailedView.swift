164ec2e06976b0a08f3f4f9905bbc3479be2e4db
Julien Hwang <jhwang@lcs.on.ca>
2024-12-12 11:53:50 -0500

    Made a detailed view for houses

---
+++ LCSHouses/Views/HouseDetailedView.swift
@@ -0,0 +1,34 @@
+//
+//  HouseDetailedView.swift
+//  LCSHouses
+//
+//  Created by Julien Hwang on 12/12/2024.
+//
+import SwiftUI
+ 
+struct HouseDetailView: View {
+    
+    // MARK: Stored properties
+    let houseToShow: House
+    
+    // MARK: Computed properties
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(houseToShow.image)
+                    .resizable()
+                    .scaledToFit()
+                    
+                Text(houseToShow.description)
+            }
+            .padding()
+        }
+        .navigationTitle(houseToShow.name)
+    }
+}
+ 
+#Preview {
+    NavigationStack {
+        HouseDetailView(houseToShow: cooper)
+    }
+}
